af4695dc684d6bd5e0f7fa7e773215c8b27bebe2
Daviel Jimenez <Daviel050399@gmail.com>
2021-01-29 23:21:29 -0400

    First Crud

---
+++ OnSale.Common/Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OnSale.Common.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
